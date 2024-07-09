a02da463c49e55b9180a057f751e993dae0dc176
Frédéric Massart <fred@branchup.tech>
2024-07-09 21:23:12 +0800

    Pre-compiling more of features during build

--- build.js
+++ build.js
@@ -1,6 +1,19 @@
 import { readFileSync, readdirSync, writeFileSync } from 'fs';
 import { JSDOM } from 'jsdom';
-import { compareVersions } from 'compare-versions';
+import { compareVersions, compare } from 'compare-versions';
+
+function getCompatibility(feature, version) {
+  const isMet = compare(version, feature.added, '>=');
+  const isRemoved = feature.removed && compare(version, feature.removed, '>=');
+  const isDeprecated = feature.deprecated && compare(version, feature.deprecated, '>=');
+  if (!isMet) {
+    return false;
+  }
+  if (isDeprecated) {
+    return 'deprecated';
+  }
+  return !isRemoved;
+}
 
 function orderByVersion(a, b) {
   if (a.added === b.added) {
@@ -12,14 +25,36 @@ function orderByVersion(a, b) {
   return compareVersions(b.added, a.added);
 }
 
+const moodleVersions = ["3.11", "4.0", "4.1", "4.2", "4.3", "4.4", "4.5"];
+
 const featureFiles = readdirSync('./features/').filter((file) => file.endsWith('.json')).map((file) => `./features/${file}`);
 const features = featureFiles.reduce((carry, file) => {
-  carry.push(JSON.parse(readFileSync(file).toString()));
+  const feature = JSON.parse(readFileSync(file).toString());
+  const compatibility = moodleVersions.reduce((carry, version) => {
+    carry[version] = getCompatibility(feature, version);
+    return carry;
+  }, {});
+
+  const available_in = Object.entries(compatibility).filter(([version, compat]) => compat === true).map(([version]) => version);
+  const deprecated_in = Object.entries(compatibility).filter(([version, compat]) => compat === 'deprecated').map(([version]) => version);
+  const unavailable_in = Object.entries(compatibility).filter(([version, compat]) => compat === false).map(([version]) => version);
+
+  carry.push({
+    ...feature,
+    id: file.replace('.json', '').replace('./features/', ''),
+    description_html: (feature.description ?? '').replace(/`([^`]+)`/g, '<code>$1</code>'),
+    notes_html: (feature.notes ?? '').replace(/`([^`]+)`/g, '<code>$1</code>'),
+    compatibility,
+    available_in,
+    deprecated_in,
+    unavailable_in,
+  });
   return carry;
 }, []).sort(orderByVersion);
 
 const dom = await JSDOM.fromFile('src/index.html');
 const document = dom.window.document;
 document.getElementById('features-json').innerHTML = JSON.stringify(features);
+document.getElementById('versions-json').innerHTML = JSON.stringify(moodleVersions);
 
 writeFileSync('dist/index.html', dom.serialize());
